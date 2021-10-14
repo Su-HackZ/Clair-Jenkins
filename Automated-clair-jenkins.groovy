3cc26dbf3083a6b6c9854e5d4b6ebc9a20e8d881
Su-HackZ <imsuperuser6@gmail.com>
2021-10-14 15:25:05 +0530

    Automated scan with report genration

--- Automated-clair-jenkins.groovy
+++ Automated-clair-jenkins.groovy
@@ -1,4 +1,5 @@
 node {
+    properties([parameters([string(defaultValue: 'ubuntu:18.04', description: 'Enter image name', name: 'image'), string(defaultValue: 'report', description: 'Enter report name', name: 'report')])])
   stage('📁️ Running Docker Container') {
     	sh """
     	sudo docker run -p 5432:5432 --rm -d --name db arminc/clair-db:latest
@@ -22,11 +23,37 @@ node {
         """     
       }
   }
-    stage('🔍️ Starting Scan') {
+    stage('🔍️ Finding Vulnerabilites') {
       //Assigning the docker ip to the DOCKER_GATEWAY
       sh'''
         DOCKER_GATEWAY=$(sudo docker network inspect bridge --format "{{range .IPAM.Config}}{{.Gateway}}{{end}}")
-        sudo ./clair-scanner --ip="$DOCKER_GATEWAY"  -r report.json ubuntu:18.04 || exit 0
+        sudo ./clair-scanner --ip="$DOCKER_GATEWAY"  -r ${report}.json ${image} || exit 0
       '''
-    }    
+      echo "Clearing container"
+      sh "sudo docker stop db clair"   
+    } 
+    stage('Converting JSON into HTML ') {    
+         sh"""
+         cat <<EOF > JSON_TO_HTML.py
+import json 
+from json2html import *
+with open('${report}.json') as f:
+    d = json.load(f)
+    #converting and assign json to scanOutput
+    scanOutput = json2html.convert(json=d)
+    htmlReportFile = "${report}.html"
+    with open(htmlReportFile, 'w') as htmlfile:
+        htmlfile.write(str(scanOutput))
+        print("Json file is converted into html")
+EOF
+         """
+         echo "Configure python"
+         sh"""
+            sudo apt-get install python3 -y && sudo apt-get install python3-pip -y
+            sudo pip install json2html
+            sudo python3 JSON_TO_HTML.py
+          """
+          sh"ls"
+         
+    }   
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Clair-Jenkins
+
+Intigration of jenkins and clair using docker to perform security scans on docker containers
